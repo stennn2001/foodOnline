f6d36f15c60f051792207fcf4f983ae7fc3fdafd
stennn <stennn@msn.com>
2024-02-06 17:20:03 +0200

    register user functionalities and homepage setup

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,16 +9,14 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print("user profile is created")
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
         except:
             UserProfile.objects.create(user=instance)
-            print("Profile did not exist, so it was created")
-        print("user is updated")
+
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, "this user is being saved")
+    pass
