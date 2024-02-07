1bfeb2a604719d04e5c4e892c8c6fbd591371b6a
stennn <stennn@msn.com>
2024-02-07 14:34:27 +0200

    customer and vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,42 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
-from django.contrib import messages
-# Create your views here.
+from django.contrib import messages, auth
+from django.core.exceptions import PermissionDenied
+from .utils import detectUser
 
 
+# Restrict vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+# Restrict customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
 def registerUser(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in!")
+        return redirect("dashboard")
+    elif request.method == "POST":
 
         form = UserForm(request.POST)
         if form.is_valid():
             # Create the user using the form
-            #password = form.cleaned_data["password"]
-            #user = form.save(commit=False)
-            #user.set_password(password)
-            #user.role = User.CUSTOMER
-            #form.save()
+            # password = form.cleaned_data["password"]
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # form.save()
 
             # Create the user using create_user method
             first_name = form.cleaned_data["first_name"]
@@ -26,7 +44,8 @@ def registerUser(request):
             username = form.cleaned_data["username"]
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password"]
-            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
+                                            password=password)
             user.role = User.CUSTOMER
             user.save()
             messages.success(request, "Your account has been registered sucessfully!")
@@ -41,8 +60,12 @@ def registerUser(request):
     }
     return render(request, "accounts/registerUser.html", context)
 
+
 def registerVendor(request):
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in!")
+        return redirect("dashboard")
+    elif request.method == "POST":
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST, request.FILES)
         if form.is_valid() and v_form.is_valid():
@@ -73,3 +96,45 @@ def registerVendor(request):
         "v_form": v_form,
     }
     return render(request, "accounts/registerVendor.html", context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in!")
+        return redirect("myAccount")
+    elif request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in.")
+            return redirect("myAccount")
+        else:
+            messages.error(request, "Invalid login credentials.")
+            return redirect("login")
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request, "You are logged out.")
+    return redirect("login")
+
+@login_required(login_url="login")
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+@login_required(login_url="login")
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    return render(request, "accounts/custDashboard.html")
+
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request, "accounts/vendorDashboard.html")
